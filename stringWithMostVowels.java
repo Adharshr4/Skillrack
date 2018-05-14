63eefb9e9295bbe20bc5e91c6015669fbd4a60d0
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-05-14 13:42:17 +0530

    Update stringWithMostVowels.java

--- stringWithMostVowels.java
+++ stringWithMostVowels.java
@@ -26,24 +26,24 @@ public class Hello {
 	    ans=str[1];
 	}
 	int i,j,count1=0,count2=0;
-    for(i=0;i<str[0].length();i++){
-        if( str[0].charAt(i) == 'a' || str[0].charAt(i) == 'e' || str[0].charAt(i) == 'i' || str[0].charAt(i) == 'o' || str[0].charAt(i) == 'u' ){
-            count1++;
+        for(i=0;i<str[0].length();i++){
+            if( str[0].charAt(i) == 'a' || str[0].charAt(i) == 'e' || str[0].charAt(i) == 'i' || str[0].charAt(i) == 'o' || str[0].charAt(i) == 'u' ){
+                count1++;
+            }
+        }
+        for(i=0;i<str[1].length();i++){
+            if( str[1].charAt(i) =='a' || str[1].charAt(i)=='e' || str[1].charAt(i) == 'i' || str[1].charAt(i) =='o' || str[1].charAt(i) == 'u' ) {
+                count2++;
+            }
+        }
+        if(count1>count2){
+            System.out.println(str[0]);
+        }
+        else if(count2>count1){
+            System.out.println(str[1]);    
+        }
+        else{
+            System.out.println(ans);
         }
     }
-    for(i=0;i<str[1].length();i++){
-        if( str[1].charAt(i) =='a' || str[1].charAt(i)=='e' || str[1].charAt(i) == 'i' || str[1].charAt(i) =='o' || str[1].charAt(i) == 'u' ) {
-            count2++;
-        }
-    }
-    if(count1>count2){
-        System.out.println(str[0]);
-    }
-    else if(count2>count1){
-        System.out.println(str[1]);    
-    }
-    else{
-        System.out.println(ans);
-    }
-	}
 }
