39b52f831a20dcef62c8fa24d179214af325d540
Adharshr4 <36843531+Adharshr4@users.noreply.github.com>
2018-05-25 11:41:34 +0530

    Create removeS2charsFromS1.java

---
+++ removeS2charsFromS1.java
@@ -0,0 +1,26 @@
+/* given string s1 and s2 ..remove s2 characters from s1 and print
+Input:
+apple
+pan
+
+Output:
+le   */
+
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ind,i;
+		StringBuffer str1 = new StringBuffer(sc.nextLine());
+		StringBuffer str2 = new StringBuffer(sc.nextLine());
+		for(i=0;i<str2.length();i++){
+		    while(str1.indexOf(String.valueOf(str2.charAt(i))) != -1){
+		        ind = str1.indexOf(String.valueOf(str2.charAt(i)));
+		        str1.delete(ind,ind+1);
+		    }
+		}
+		System.out.println(str1);
+	}
+}
